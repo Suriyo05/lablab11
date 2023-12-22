91ec2aed92a1165a460675ab44bedc0269a3c408
suriyo <suriyo_lumsom@cmu.ac.th>
2023-12-22 16:09:27 +0700

    lab11_1.cpp complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,16 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std ;
+
+int main()
+{
+    string grade [] = {"A","B+","B","C+","C","D+","D","F","W"} ;
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    cout << "You will get "<< grade [rand()%9] << " in this 261102.";
+
+}
